9801cece9407471708d605230a11132901699b48
je7ov <jeffrey.ovalles@gmail.com>
2018-09-15 16:10:55 -0400

    Fix mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Calculator from './components/Calculator';
 class App extends Component {
   render() {
     return (
-      <div className="container d-flex flex-column h-100 justify-content-center align-items-center">
-        <h1 className="title text-center">Simple Calculator</h1>
-        <br />
-        <Calculator />
+      <div className="container h-100">
+        <div className="d-flex flex-column h-100 justify-content-center align-items-center">
+          <h1 className="title text-center">Simple Calculator</h1>
+          <br />
+          <Calculator />
+        </div>
       </div>
     );
   }
